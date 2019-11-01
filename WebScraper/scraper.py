0771d0b42f4e560155d70ed03ec038986c77f6bf
Alexander Sun <technoal9@gmail.com>
2019-11-01 11:56:28 -0400

    added newsapi

--- WebScraper/scraper.py
+++ WebScraper/scraper.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from newsapi import NewsApiClient
 
-page = requests.get("https://google.com/search?q=facebook+news" + "&as_qdr=y15")
-if page.status_code == 200:
-    soup = BeautifulSoup(page.content, "lxml")
-    print(soup.prettify())
-    #print(list(soup.children))
-    all_tags = soup.find_all('p')
-    for i in all_tags:
-        print(i.get_text())
-else:
-    print("unfortunate failure")
+import requests
+url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&'
+       'from=2019-10-31&'
+       'sortBy=popularity&'
+       'apiKey=13bd628fa8b548738d3b113d9442574e')
+
+response = requests.get(url)
+
+print(response.json())
+# page = requests.get("https://google.com/search?q=facebook+news" + "&as_qdr=y15")
+# if page.status_code == 200:
+#     soup = BeautifulSoup(page.content, "lxml")
+#     print(soup.prettify())
+#     #print(list(soup.children))
+#     all_tags = soup.find_all('p')
+#     for i in all_tags:
+#         print(i.get_text())gi
+# else:
+#     print("unfortunate failure")
