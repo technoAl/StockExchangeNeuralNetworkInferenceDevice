a7e7393b76a9cdd83f1ec1b45b16ebeb89416d54
Alexander Sun <technoal9@gmail.com>
2019-11-05 08:47:42 -0500

    web scraper

--- WebScraper/scraper.py
+++ WebScraper/scraper.py
@@ -1,24 +1,36 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from newsapi import NewsApiClient
 
+
 import requests
+
+query = 'tesla'
 url = ('https://newsapi.org/v2/everything?'
-       'q=Apple&'
+       'q=' + query + '&'
        'from=2019-10-31&'
        'sortBy=popularity&'
-       'apiKey=13bd628fa8b548738d3b113d9442574e')
+       'apiKey=13bd628fa8b548738d3b113d9442574e&'
+       'pageSize=40&'
+       'language=en')
 
 response = requests.get(url)
-
-print(response.json())
-# page = requests.get("https://google.com/search?q=facebook+news" + "&as_qdr=y15")
-# if page.status_code == 200:
-#     soup = BeautifulSoup(page.content, "lxml")
-#     print(soup.prettify())
-#     #print(list(soup.children))
-#     all_tags = soup.find_all('p')
-#     for i in all_tags:
-#         print(i.get_text())gi
-# else:
-#     print("unfortunate failure")
+data = response.json()
+print(data['totalResults'])
+data = data['articles']
+print(len(data))
+count = 0
+for i in data:
+       tmpDict = dict(i)
+       file = open(query + str(count) + '.txt', 'w+')
+       page = requests.get(tmpDict['url'])
+       if page.status_code == 200:
+              soup = BeautifulSoup(page.content, "lxml")
+              all_tags = soup.find_all('p')
+              for i in all_tags:
+                     file.write(i.get_text())
+       else:
+              print("unfortunate failure")
+       file.close()
+       count+=1
