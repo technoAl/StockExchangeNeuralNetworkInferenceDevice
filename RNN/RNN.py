f47048cd00da66b2fd35f086cb4eaa1cdcb58b93
Darshan Krishnaswamy <darsh797@gmail.com>
2019-11-13 07:50:23 -0500

    Hopefully doesnt push idea stuff

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow_datasets as tfds
 import tensorflow as tf
