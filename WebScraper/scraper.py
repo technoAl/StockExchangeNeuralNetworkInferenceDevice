7a71802c9229e1fada084b48cb16d3dbf4dac5e0
Alexander Sun <technoal9@gmail.com>
2019-11-25 09:10:58 -0500

    fixed a borken thing

--- WebScraper/scraper.py
+++ WebScraper/scraper.py
@@ -32,10 +32,10 @@ def obtainTrainingData(code, year, month, day):
        print(len(data))
        count = 0
        path = '../'+ 'TrainingData' + '/' + query + 'TrainingData' + '_' +  date +'/'
-       for i in data:(i)
+       for i in data:
+              tmpDict = dict(i)
               fullName = path + query + str(count) + '.txt'
-              os.makedirs(os.p
-              tmpDict = dictath.dirname(fullName),exist_ok=True)
+              os.makedirs(os.path.dirname(fullName),exist_ok=True)
               file = open(fullName, 'w')
               try:
                      page = requests.get(tmpDict['url'])
